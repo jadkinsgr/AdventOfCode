4c49c9dd9ae26cbfbd85fb7af8285fe3edf234f2
Josh Adkins <joshua.adkins@healthcatalyst.com>
2022-12-01 22:39:09 -0500

    nomenclature updated

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -2,18 +2,18 @@
 def get_input(file_name):
 #special encoding for my sensitive mac 
     with open(file_name,  encoding='utf-8-sig') as f:
-        data = [elf.splitlines() for elf in f.read().split('\n\n')]
-    data = [[int(meal) for meal in elf] for elf in data]
+        data = [item.splitlines() for item in f.read().split('\n\n')]
+    data = [[int(calorie) for calorie in item] for item in data]
     return data
 
-#build part one, which is to get the sum of all values per elf and show the top value
+#build part one, which is to get the sum of all values per item and show the top value
 def part_one(data):
-    maximum = max([sum(elf) for elf in data])
+    maximum = max([sum(item) for item in data])
     return maximum
 
 #build part two which is to sort the top values in reverse order and then sum up the top 3
 def part_two(data):
-    return sum(sorted([sum(elf) for elf in data], reverse=True)[:3])
+    return sum(sorted([sum(item) for item in data], reverse=True)[:3])
 
 #pulling it
 def main():
